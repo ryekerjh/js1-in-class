a38035101e235560cfbf33b809401f611ed88350
Ryeker <ryekerjh@gmail.com>
2017-11-01 18:06:16 -0700

    day 1 in class practice examples

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+var name = "Ryeker";
+
+//ES5 String Interpolation
+var message = "Hello there, " + name + "!";
+console.log(message);
+
+//ES6 STring Interpolation
+var secondMessage = `Hello there, ${name}!`;
+console.log(secondMessage);
+
+
+
+
+//Variable declaration
+console.log(number);
+
+ var number = 14;
+//Variable Definition
+console.log("My goal is to....");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
