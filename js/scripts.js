63f87a2a2060e957cbb3fc2031a4dbd6a7a8ff26
Ryeker <ryekerjh@gmail.com>
2017-11-06 18:26:18 -0800

    day 2 takehome

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,19 @@
-// var message = "hellow world!";
-// console.log(typeof message);
-// var john = 23;
-// console.log(typeof john);
-// var bool = true;
-// 
-let fresh = true, otherVar = 0;
-// 
-// console.log(fresh, otherVar);
-// console.log(name);
-if(fresh) {
-  const name = "Peter";
-  // console.log(name);
-};
+/*Create a file named app.js
+Write a program that:
+Asks for the user's first name
+Asks for the user's last name
+Logs the user's first name to the console
+Alerts the user's last name
+*Confirms the user's birthday and alerts the resulting birthday
+Include the app.js in an empty html page
+Add it to a repository's gh-pages branch so I can test it
+*/
 
-var name = "Ryan";
-
-// console.log(name);
-
-// const best = 77;
-// var best = 'Star Wars';
-// let best = 33;
-// console.log(best);
-// 
-// var rightNow = new Date();
-// 
-// 
-// var randomNumber = Math.random();
-// 
-// var name = 'John';
-// 
-var stringObj = new String('hello');
-
-// console.log(typeof name, typeof stringObj);
-
-// console.log(stringObj);
-
-let bestNumber = 42;
-
-// console.log(typeof bestNumber);
-
-let reallyBestNumber = new Number(42);
-
-// console.log(reallyBestNumber);
-
-let bool = new Boolean(true);
-
-// console.log(bool);
-
-let object = {
-  firstName: 'Stephen', 
-  lastName: 'johnstone', 
-  age: 32
-};
-// console.log(object);
-
-// alert("whoa, hey there!");
-// var confirm = confirm("Are you sure?");
-// console.log(confirm);
-var userAnswer = prompt("What is your name?", "Johnny (first name only!!)");
-console.log(userAnswer);
+let firstName = prompt('What\'s your first name?');
+console.log(firstName);
+let lastName = prompt("What's your last name?");
+alert(lastName);
+let birthday = prompt('What is your birthday?', '12/12/2000');
+let isBirthdayCorrect = confirm(`Is ${birthday} your birthday?`);
+//Assuming user said "yes"
+alert(birthday);
