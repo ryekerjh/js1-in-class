8c9aac61886ca20b25c734744608cd231077f92a
Ryeker <ryekerjh@gmail.com>
2017-11-06 17:58:25 -0800

    day 2 in class details

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+// var message = "hellow world!";
+// console.log(typeof message);
+// var john = 23;
+// console.log(typeof john);
+// var bool = true;
+// 
+let fresh = true, otherVar = 0;
+// 
+// console.log(fresh, otherVar);
+// console.log(name);
+if(fresh) {
+  const name = "Peter";
+  // console.log(name);
+};
+
+var name = "Ryan";
+
+// console.log(name);
+
+// const best = 77;
+// var best = 'Star Wars';
+// let best = 33;
+// console.log(best);
+// 
+// var rightNow = new Date();
+// 
+// 
+// var randomNumber = Math.random();
+// 
+// var name = 'John';
+// 
+var stringObj = new String('hello');
+
+// console.log(typeof name, typeof stringObj);
+
+// console.log(stringObj);
+
+let bestNumber = 42;
+
+// console.log(typeof bestNumber);
+
+let reallyBestNumber = new Number(42);
+
+// console.log(reallyBestNumber);
+
+let bool = new Boolean(true);
+
+// console.log(bool);
+
+let object = {
+  firstName: 'Stephen', 
+  lastName: 'johnstone', 
+  age: 32
+};
+// console.log(object);
+
+// alert("whoa, hey there!");
+// var confirm = confirm("Are you sure?");
+// console.log(confirm);
+var userAnswer = prompt("What is your name?", "Johnny (first name only!!)");
+console.log(userAnswer);
