074ac90cf6a4b135756e7b358d060cf4cfea4a64
Ryeker <ryekerjh@gmail.com>
2017-11-28 21:33:45 -0800

    basic funcitonality complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 const btns = document.getElementsByClassName('numbs');
 const operators = document.getElementsByClassName('ops');
 var result = document.getElementById('result'),
-total = 0,
-valueShown = 0;
-selectedValue = "0",
-firstValue = "",
-secondValue = "",
+firstValue = 0,
+secondValue = 0,
 allowSecond = false,
-operatorType = null;
-result.innerText = total;
+secondEntered = false,
+operatorType = null,
+memory = null;
+result.innerText = 0;
 
 for(let i = 0; i < btns.length; i++) {
   let thisBtn = btns[i];
@@ -16,48 +15,121 @@ for(let i = 0; i < btns.length; i++) {
   thisBtn.value = thisBtn.innerText;
   thisBtn.addEventListener("click", (e) => {
     if(!allowSecond) {
-      firstValue == "0" ? firstValue = e.target.innerText.toString() : firstValue += e.target.innerText.toString();
-      valueShown = firstValue;
-      total = firstValue;
-      result.innerText = valueShown;
+      setFirst(parseInt(`${e.target.value}`));
     } else {
-      secondValue += e.target.innerText.toString();
-      valueShown = secondValue;
-      result.innerText = valueShown;
+      setSecond(parseInt(`${e.target.value}`));
     }
   }); 
 }
 
 for(let j = 0; j < operators.length; j++) {
   let thisBtn = operators[j];
-  thisBtn.addEventListener('click', (e) => {
-    firstValue = parseInt(firstValue);
-    secondValue = parseInt(secondValue);
-  if(operatorType == null) {
-    operatorType = `${e.target.innerText}`;
+  thisBtn.addEventListener('click', function(e) {
     allowSecond = true;
-    console.log(operatorType);
-  } else if(operatorType == '=') {
-      console.log('first', firstValue, 'second', secondValue);
-        switch(operatorType) {
-          case '+':
-          total = firstValue + secondValue;
-          console.log(total);
-          break;
-          case '-':
-          total = firstValue - secondValue;
-          break;
-          case '*':
-          total = firstValue * secondValue;
-          break;
-          case '/':
-          total = firstValue / secondValue;
-          break;
-          default:
-          total = total;
-          break;
-        }
-      }
-    result.innerText = total;
-  })
+    if(secondValue !== 0) {
+      performOperation(`${e.target.innerText}`)
+    } else {
+      operatorType = `${e.target.innerText}`;
+    }
+  });
 }
+
+function setFirst(value) {
+  if(firstValue != 0) {
+    firstValue += value.toString();
+  } else {
+    firstValue = value.toString();
+  }
+  result.innerText = firstValue;
+};
+
+function setSecond(value) {
+  if(secondValue != 0) {
+    secondValue += value.toString();
+  } else {
+    secondValue = value.toString();
+  }
+  secondEntered = true;
+  result.innerText = secondValue;
+};
+
+function performOperation(operator) {
+  firstValue = parseInt(firstValue);
+  secondValue = parseInt(secondValue);
+  switch(operator) {
+    case '+':
+      firstValue += secondValue;
+      operatorType = '+';
+      secondValue = 0;
+      break;
+    case '-':
+      firstValue -= secondValue;
+      operatorType = '-';
+      secondValue = 0;
+      break;
+    case '*':
+      multiply();
+      break;
+    case '/':
+      divide();
+      break;
+    case 'C':
+      clearValues();
+      break;
+    default:
+      evaluateEquals();
+      break;
+  }
+  result.innerText = firstValue;
+};
+
+function evaluateEquals() {
+  console.log(operatorType, 'optype', firstValue, '<<<first', secondValue, '<<<second');
+  switch(operatorType) {
+    case '+':
+      firstValue += secondValue;
+      break;
+    case '-':
+      firstValue -= secondValue;
+      break;
+    case '*':
+      multiply();
+      break;
+    case '/':
+      divide();
+      break;
+    default:
+      firstValue = firstValue;
+      break;
+  }
+  result.innerText = firstValue;
+  // secondValue = 0;
+  secondEntered = false;
+}
+
+function multiply() {
+  if(secondEntered) {
+    firstValue *= secondValue;
+    secondValue = 0;
+  } else {
+    firstValue = firstValue;
+  }
+  operatorType = '*';
+};
+
+function divide() {
+  if(secondEntered) {
+    firstValue /= secondValue;
+    secondValue = 0;
+  } else {
+    firstValue = firstValue;
+  }
+  operatorType = '/';
+};
+
+function clearValues() {
+  firstValue = "0";
+  secondValue = "0";
+  operatorType = null;
+  allowSecond = false;
+};
